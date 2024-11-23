14cc100da96a39e83295752f6469eb220af836d8
LeoAnders <leonardoanders15@gmail.com>
2024-11-23 14:40:08 -0300

    Criado regex dos parâmetros

--- src/server.js
+++ src/server.js
@@ -9,10 +9,14 @@ const server = http.createServer(async (req, res) => {
   await json(req, res)
 
   const route = routes.find(route => {
-    return route.method === method && route.path === url
+    return route.method === method && route.path.test(url)
   })
 
   if (route) {
+    const routeParams = req.url.match(route.path)
+
+    console.log(routeParams);
+
     return route.handler(req, res)
   }
 
@@ -20,4 +24,4 @@ const server = http.createServer(async (req, res) => {
   return res.writeHead(404).end()
 })
 
-server.listen(3333)
+server.listen(3333) 
